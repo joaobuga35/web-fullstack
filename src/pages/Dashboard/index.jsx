8e10a606d032aed73d2d4322353453e1c3a04e69
joaobuga35 <joaobuga35@gmail.com>
2023-07-22 01:18:32 -0300

    feat: create input search with animation and finishing card notion 1:1

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,17 +1,40 @@
+import { useState } from "react";
 import CardOneToOne from "../../components/Cards";
+import CardNotion from "../../components/Cards/CardNotion";
 import CardTalk from "../../components/Cards/CardTalk";
 import Header from "../../components/Header";
 import search from "../../images/svg/search.svg";
 import { ButtonBlue, ButtonDefault } from "../../styles/Buttons/styles";
-import { DivButtonsFilter, MainContentDash, SectionTitle } from "./styles";
+import {
+  DivButtonsFilter,
+  MainContentDash,
+  SearchContainer,
+  SearchInput,
+  SectionTitle,
+} from "./styles";
+import { FiChevronLeft } from "react-icons/fi";
 
 const Dashboard = () => {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
   return (
     <>
       <Header />
+      {isSearchOpen && (
+        <SearchContainer>
+          <button onClick={() => setIsSearchOpen(false)}>
+            {" "}
+            <FiChevronLeft />
+          </button>
+          <SearchInput type="text" placeholder="Pesquisar..." />
+        </SearchContainer>
+      )}
       <SectionTitle>
         <h2>Conversas 1:1</h2>
-        <img src={search} alt="pesquisa" />
+        <img
+          onClick={() => setIsSearchOpen(true)}
+          src={search}
+          alt="pesquisa"
+        />
       </SectionTitle>
 
       <DivButtonsFilter>
@@ -27,6 +50,7 @@ const Dashboard = () => {
         <CardOneToOne />
         <CardOneToOne />
         <CardTalk />
+        <CardNotion />
       </MainContentDash>
     </>
   );
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,4 +1,15 @@
-import { styled } from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const fadeInAnimation = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
 
 const SectionTitle = styled.section`
   display: flex;
@@ -16,6 +27,9 @@ const SectionTitle = styled.section`
     line-height: 24.286px;
     letter-spacing: 0.101px;
   }
+  img {
+    cursor: pointer;
+  }
 `;
 
 const DivButtonsFilter = styled.div`
@@ -32,4 +46,48 @@ const MainContentDash = styled.div`
   padding-right: 16px;
 `;
 
-export { SectionTitle, DivButtonsFilter, MainContentDash };
+const SearchContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  background-color: var(--color-white);
+  padding: 20px;
+  border-radius: 5px;
+  width: 100%;
+  height: 80px;
+  position: absolute;
+  top: 143px;
+  right: 1px;
+  z-index: 1;
+  animation: ${fadeInAnimation} 0.3s ease-in-out;
+
+  button {
+    width: 27px;
+    height: 27px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100%;
+    border: 1px solid var(--color-primary);
+    background-color: var(--color-white);
+    color: var(--color-primary);
+    font-size: 16px;
+  }
+`;
+
+const SearchInput = styled.input`
+  border-radius: 60px;
+  border: 1px solid #e8e8e8;
+  background: #fff;
+  width: 90%;
+  height: 45px;
+  padding: 13px 23px 12px 12px;
+`;
+
+export {
+  SectionTitle,
+  DivButtonsFilter,
+  MainContentDash,
+  SearchContainer,
+  SearchInput,
+};

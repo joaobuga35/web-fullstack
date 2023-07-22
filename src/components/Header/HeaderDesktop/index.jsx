5990a672d7d8867f47c1f8973b6d455d3727a04e
joaobuga35 <joaobuga35@gmail.com>
2023-07-22 15:50:54 -0300

    feat: finishing header desktop

--- src/components/Header/HeaderDesktop/index.jsx
+++ src/components/Header/HeaderDesktop/index.jsx
@@ -1,9 +1,17 @@
-import { HeaderDeskStyle } from "./styles";
+import { DivProfileHeader, HeaderDeskStyle } from "./styles";
+import profile from "../../../images/png/profile.png";
+import { HiChevronDown } from "react-icons/hi";
 
 const HeaderDesktop = () => {
   return (
     <HeaderDeskStyle>
-      <h1>Header desktop</h1>
+      <h1>Conversas 1:1</h1>
+
+      <DivProfileHeader>
+        <img src={profile} alt="perfil" />
+        <span>Mateus Barbosa</span>
+        <HiChevronDown />
+      </DivProfileHeader>
     </HeaderDeskStyle>
   );
 };
--- src/styles/Container/styles.js
+++ src/styles/Container/styles.js
@@ -1,12 +1,15 @@
 import { styled } from "styled-components";
 
 const ContainerContentDesktop = styled.div`
+  background-color: #f7f9fb;
   display: flex;
 `;
 
 const MainContentDesktop = styled.main`
   flex: 1;
   display: flex;
+  padding-right: 70px;
+  padding-left: 70px;
 `;
 
 export { ContainerContentDesktop, MainContentDesktop };
